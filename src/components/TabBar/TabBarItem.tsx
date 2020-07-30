27104be26f0d8187953079ff5443c1586efd3c3a
zhoulei <11893927@qq.com>
2020-07-30 18:17:21 +0800

    optimization code

--- src/components/TabBar/TabBarItem.tsx
+++ src/components/TabBar/TabBarItem.tsx
@@ -13,7 +13,7 @@ export interface IProps{
 }
 
 export interface IColorStyle{
-  color: string | undefined;
+  color?: string;
 }
 
 const TabBarItem: React.FC<IProps> = (props) => {
@@ -21,7 +21,7 @@ const TabBarItem: React.FC<IProps> = (props) => {
   return (<Route path={path} exact children={(props) => {
     const style: IColorStyle = {color: props.match ? selectedColor : color}
     return (<Link to={path} className="tab-bar-item flex-1 d-flex fd-column jc-center ai-center">
-      {iconPath ? <img className="icon-img" src={props.match ? selectedIconPath : iconPath} alt="图片"/> : null}
+      {iconPath ? <img className="icon-img" src={props.match ? selectedIconPath : iconPath} alt="icon"/> : null}
       {icon ? <i className={icon} style={style}></i> : null}
       {text ? <span className="text" style={style}>{text}</span> : null}
     </Link>)
@@ -30,7 +30,7 @@ const TabBarItem: React.FC<IProps> = (props) => {
 }
 
 TabBarItem.defaultProps = {
-  color: "#000",
+  color: "#777",
   selectedColor: '#5c7efa'
 }
 
